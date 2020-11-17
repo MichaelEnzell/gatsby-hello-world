64d2d4bb6cc24ec23ecf494c58406d866260804a
Michael Enzell <Michael.Enzell@fenixoutdoor.se>
2020-11-17 10:19:01 +0100

    Added container and user modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import React from "react"
 import Header from "../components/header"
+import Container from "../components/container"
+import User from "../components/user"
 import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <Container>
+      <Link to="/">Home</Link>
       <Header headerText="Hello world!" />
-      <Link to="/contact/">Contact</Link>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  );
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe, globally known but literally unknown."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy."
+      />
+    </Container>
+  )
 }
